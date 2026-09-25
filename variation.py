9e330d58a0c866450ef9b655f1f8fe720d7aa433
atharvdashpute2 <atharvdashpute2@gmail.com>
2026-09-25 23:03:23 +0530

    Refactor image variation generation using OpenAI client
    Refactored code to use OpenAI client for image editing and added functionality to create a variation of an image.

--- variation.py
+++ variation.py
@@ -1,16 +1,24 @@
-import openai
+from openai import OpenAI
 from dotenv import load_dotenv
 import os
+import base64
 
 load_dotenv()
 
-openai.api_key = os.environ["API_KEY"]
-
-response = openai.Image.create_variation(
-  image=open("images/img-dev.png", "rb"),
-  n=1,
-  size="1024x1024"
+client = OpenAI(
+    api_key=os.environ["API_KEY"]
 )
-image_url = response['data'][0]['url']
 
-print(image_url)
+with open("images/img-dev.png", "rb") as image_file:
+    response = client.images.edit(
+        model="gpt-image-1",
+        image=image_file,
+        prompt="Create a new variation of this image while keeping the main subject and composition similar."
+    )
+
+image_data = base64.b64decode(response.data[0].b64_json)
+
+with open("images/img-dev-variation.png", "wb") as file:
+    file.write(image_data)
+
+print("Image variation generated successfully!")
